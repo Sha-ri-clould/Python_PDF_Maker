e2ee83770c07aeaed806444d9456296de6dce2ab
SHARIGA P <sharigapandian24@gmail.com>
2025-05-14 19:27:20 +0530

    Added pages for each topic

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ for index,row in df.iterrows():
     pdf.set_font(family='Times', size=26, style='B')
     pdf.cell(w=0,h=12,txt=row['Topic'],align="L",ln=1,border=0)
     pdf.line(10,21,200,21)
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 
 
